ebe4208a26cf117c134c4706ac74d6fe394ac778
Ashmita <kca1@nku.edu>
2022-03-20 18:58:45 -0400

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by student on 3/20/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
